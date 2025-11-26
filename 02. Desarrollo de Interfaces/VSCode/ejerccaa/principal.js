c742fedf51d854709d7dc5321ddc62dbfa60aa67
Chengzhe Li <chengzhe17415@gmail.com>
2025-11-26 01:13:19 +0100

    Terminado Ejercicio Provincias JS

--- 02. Desarrollo de Interfaces/VSCode/ejerccaa/principal.js
+++ 02. Desarrollo de Interfaces/VSCode/ejerccaa/principal.js
@@ -1,15 +1,38 @@
 import {provincias, cautonomas} from './autonomias-provincias.js';
 
-const marcoProv = document.getElementById("marcoProvincias");
-cautonomas.forEach((_, idx)=>{
-    ponerImagen(idx,marcoProv);
-});
+function inicio(){
+    const marcoProv = document.getElementById("marcoProvincias");
+    cautonomas.forEach((_, idx)=>ponerImagen(idx,marcoProv));
+}
 
 function ponerImagen(pos,des){
     let elem = document.createElement("img");
+    let id = "imagen_"+pos;
     elem.setAttribute("src","banderas/" + cautonomas[pos] + ".gif");
-    elem.setAttribute("height","50");
-    elem.setAttribute("width","50");
-    elem.setAttribute("id","imagen_"+pos);
+    elem.setAttribute("id", id);
+    elem.addEventListener("click",()=>hacerClick(elem, pos));
     des.appendChild(elem);
 }
+
+function hacerClick(elem, pos){
+    //Quitar la clase hechoClick al anterior
+    const anterior = document.querySelector(".hechoClick");
+    if(anterior !== null)anterior.classList.remove("hechoClick");
+    //Añadir la clase hechoClick al elemento.
+    elem.classList.add("hechoClick");
+
+    //Actualizar el fieldSet con los datos
+    document.getElementById("leyenda").innerHTML = cautonomas[pos];
+
+    let selector = document.getElementById("ccaa");
+    selector.innerHTML = "";
+    provincias[pos].forEach(provincia=>{
+        let opcion = document.createElement("option");
+        opcion.value = provincia;
+        opcion.innerHTML = provincia;
+        selector.appendChild(opcion);
+    });
+}
+
+
+window.onload = ()=>(inicio());
